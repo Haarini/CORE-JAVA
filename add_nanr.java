a8aae5b7bfb55514255d06e3bc9afd3beb829941
Haarini Rao <39938842+Haarini@users.noreply.github.com>
2018-08-12 23:40:40 +0530

    Create add_nanr.java

---
+++ add_nanr.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.util.Scanner;
+//Fucntion with no arguement and no return type
+class add_nanr
+{  
+    void add()
+    {
+        int a,b;
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter numbers to be added:");
+        a=s.nextInt();
+        b=s.nextInt();
+        int c;
+        c=a+b;
+        System.out.println("Sum is "+c);
+
+       
+    }
+        
+    public static void main(String args[])
+    {
+        
+        Rextester o=new Rextester();
+        o.add();
+     
+}
+    }
